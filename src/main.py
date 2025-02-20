d8afdfb89e0438ea25b6ba0c6399001e43063a23
fathi <fathimhiri10@gmail.com>
2025-02-20 17:32:06 +0100

    upload file route with validation

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from routes import base
-from dotenv import load_dotenv
-load_dotenv(".env")
+from routes import base,data
+#from dotenv import load_dotenv
+#load_dotenv(".env")
 
 app = FastAPI()
 app.include_router(base.base_router)
-
+app.include_router(data.data_router)
 
 #@app.get("/w")
 #def read_root():
